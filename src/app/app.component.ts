ce959367ca5ae238d903a352b57126461b006c9b
spa51 <santi.15elspa@outlook.com>
2024-02-11 15:36:15 -0500

    Preajustes angular boostrap y leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as  Leaflet from 'leaflet';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeafletModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
