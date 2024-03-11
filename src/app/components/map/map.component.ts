87f959590319b15b4fe054d80c82e3f5e2208665
spa51 <santi.15elspa@outlook.com>
2024-03-10 22:21:53 -0500

    mapa funcional

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import * as  Leaflet from 'leaflet';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LocationService } from '../../services/location.service';
 
 @Component({
   selector: 'app-map',
@@ -19,19 +20,19 @@ export class MapComponent {
     zoom: 15,
     center: Leaflet.latLng(6.247980096310698, -75.57650510205677)
   };
-  layers = [
-    Leaflet.marker([ 6.247980096310698, -75.57650510205677]).on('click', this.MapMarkerClick).bindPopup('<div class="custom-popup">Texto a mostrar <br> Hola</br></div>', {
-      className: 'custom-popup',
-      minWidth: 200, // Ancho mínimo del popup
-      maxWidth: 400, // Ancho máximo del popup
-      maxHeight: 200, // Altura máxima del popup
-      autoPan: true, // Desplazar automáticamente el mapa para mostrar el popup
-      closeButton: true, // Mostrar botón de cierre en el popup
-      closeOnClick: true // Cerrar el popup al hacer clic fuera de él
+  layers:Leaflet.Layer[] = [];
+
+  constructor(private locationService: LocationService){
+
+  }
+
+  ngOnInit():void{
+    this.locationService.getListLocations().subscribe(locations =>{
+      locations.forEach(location => {
+        this.layers.push(Leaflet.marker([location.latitude, location.longitude])
+        .bindPopup(`<b>${location.name}</b><br>${location.description}`))
+      })
     })
-  ];
-MapMarkerClick(event: any){
-  ('Hola soy un marcador');
-}
+  }
 
 }
