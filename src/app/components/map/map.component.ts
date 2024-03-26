1acd518d5f478e938e43eb07de2b2a114eb2c929
spa51 <santi.15elspa@outlook.com>
2024-03-26 16:27:20 -0500

    se añadieron diferentes diseños de mapas temporal

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -4,6 +4,7 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { LocationService } from '../../services/location.service';
 import { CategoryService } from '../../services/category.service';
 import { Category } from '../../interfaces/category';
+import { LeafletControlLayersConfig } from '@asymmetrik/ngx-leaflet';
 
 @Component({
   selector: 'app-map',
@@ -14,48 +15,67 @@ import { Category } from '../../interfaces/category';
 })
 export class MapComponent {
   title = 'Front-End';
-  options = {
-    layers: [
-      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
+  map!: Leaflet.Map;
+  baseLayers: Leaflet.Layer[] = [];// Define las capas base
+  layersControl: LeafletControlLayersConfig;
 
+  options = {
     zoom: 15,
     center: Leaflet.latLng(6.247980096310698, -75.57650510205677)
   };
-  layers:Leaflet.Layer[] = [];
+  
+  constructor(private locationService: LocationService, private categoryService: CategoryService) {
+    const openStreetMapLayer = Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'OpenStreetMap' });
+    const googleMapsLayer = Leaflet.tileLayer('https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}', { attribution: 'Google Maps' });
+    const googleSatelliteLayer = Leaflet.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', { attribution: 'Google Satellite' });
+    const googleHybridLayer = Leaflet.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', { attribution: 'Google Hybrid' });
 
-  constructor(private locationService: LocationService, private categoryService: CategoryService){
+    this.baseLayers = [openStreetMapLayer, googleMapsLayer, googleSatelliteLayer, googleHybridLayer];
 
+    this.layersControl = {
+      baseLayers: {
+        'OpenStreetMap': openStreetMapLayer,
+        'Google Maps': googleMapsLayer,
+        'Google Satellite': googleSatelliteLayer,
+        'Google Hybrid': googleHybridLayer
+      },
+      overlays: {}
+    };
   }
+  
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.locationService.getListLocations().subscribe(locations => {
       locations.forEach(location => {
         this.categoryService.getCategory(location.category).subscribe(category => {
           this.getCustomIcon(category).then(customIcon => {
-            this.layers.push(
-              Leaflet.marker([location.latitude, location.longitude], { icon: customIcon })
-                .bindPopup(`<b>${location.name}</b><br>${location.description}`)
-            );
+            const marker = Leaflet.marker([location.latitude, location.longitude], { icon: customIcon })
+              .bindPopup(`<b>${location.name}</b><br>${location.description}`);
+            marker.addTo(this.map);
           });
         });
       });
     });
   }
 
-    async getCustomIcon(category: Category): Promise<Leaflet.Icon> {
-      if (category && category.iconUrl) {
-        return Leaflet.icon({
-          iconUrl: category.iconUrl,
-          iconSize: [41, 31],
-          iconAnchor: [15, 38]
-        });
-      } else {
-        return Leaflet.icon({
-          iconUrl: '/assets/icons/markers/default-marker.svg',
-          iconSize: [41, 31],
-          iconAnchor: [15, 38]
-        });
-      }
+
+  onMapReady(map: Leaflet.Map) {
+    this.map = map;
+  }
+
+  async getCustomIcon(category: Category): Promise<Leaflet.Icon> {
+    if (category && category.iconUrl) {
+      return Leaflet.icon({
+        iconUrl: category.iconUrl,
+        iconSize: [41, 31],
+        iconAnchor: [15, 38]
+      });
+    } else {
+      return Leaflet.icon({
+        iconUrl: '/assets/icons/markers/default-marker.svg',
+        iconSize: [41, 31],
+        iconAnchor: [15, 38]
+      });
     }
   }
+}
