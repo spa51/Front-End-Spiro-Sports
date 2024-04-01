78dac9b8b9c2529694fba4a6143c4cac4cfafd91
spa51 <santi.15elspa@outlook.com>
2024-03-31 21:38:48 -0500

    icono de login mejorado

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('token'); // Retorna true si hay un token
+  }
 
   constructor(private router: Router){
 
